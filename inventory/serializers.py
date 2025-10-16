999c26949f04f8e6abd3da4cf33b496d5f77fb5e
Carlos Valeriano <carlos.valeriano@tecsup.edu.pe>
2025-10-15 22:19:02 -0500

    Views
    oño 4to commit

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Product, Category
 
 class CategorySerializer(serializers.ModelSerializer):
-    # Mostrar cantidad de productos en la categoría (opcional)
     products_count = serializers.IntegerField(source='products.count', read_only=True)
 
     class Meta:
@@ -10,7 +9,6 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description', 'products_count']
 
 class ProductSerializer(serializers.ModelSerializer):
-    # campo opcional extra: nombre de la categoría embebido para mejor UX (punto extra)
     category_name = serializers.CharField(source='category.name', read_only=True)
 
     class Meta:
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+
+    queryset = Product.objects.select_related('category').all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['category', 'quantity']
+    search_fields = ['name', 'category__name']
+    ordering_fields = ['name', 'price', 'quantity']
+
+class CategoryViewSet(viewsets.ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['name']
